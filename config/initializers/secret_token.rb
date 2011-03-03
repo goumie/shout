a2c55783bfd70e6ae8d5b50b964dfd9096a9360e
CG <Personal@Administrators-MacBook-Pro.local>
2011-03-03 13:20:56 -0500

    1st

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shout::Application.config.secret_token = '0840ec43051ec4339fc4f9cfe6d9aece9b5f7e694a175ec4fc51394995544aa0bfa538a28bf5dc727ff2c4415d09384359c6624ac909f7c99591c22bfcb1563f'
